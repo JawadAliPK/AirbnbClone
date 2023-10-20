d5217732c4583044a1879963e210c4d7de8db9f8
Jawad Ali <jawad@eddress.co>
2023-10-20 20:08:18 +0200

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by jawad ali on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
